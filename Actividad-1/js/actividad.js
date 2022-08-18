0c29d4aae711a3e9f27ebce530afe33207c2bd40
Marcos <marincon444@misena.edu.co>
2022-08-18 17:11:50 -0500

    Actividad 1

---
+++ Actividad-1/js/actividad.js
@@ -0,0 +1,26 @@
+// let dinero, total = 0;
+
+// do{
+//     dinero = parseInt( prompt("Cuando dinero quiere ingresar:"));
+//     total=total+dinero;
+//     respuesta=parseInt(prompt("¿Desea ingresar mas dinero? 1 para Si y 2 para No"))
+
+// }while(respuesta==1)
+
+// alert("Tiene un total: "+total)
+// alert("Fin del proceso")
+
+
+// let peso, respuesta;
+// peso = parseInt(prompt("Ingrese el valor en pesos"))
+// respuesta= parseInt(prompt("Desea conocer el precio en: oprime 1 a dolares , y oprime 2 a euros"))
+
+// if(respuesta==1){
+// total = peso/3000;
+// alert(peso+" pesos son "+total+" dolares");
+// }else if(respuesta==2){
+// total = peso/3500;
+// alert(peso+" pesos son "+total+" euros");
+// }
+// alert("Fin del proceso")
+
