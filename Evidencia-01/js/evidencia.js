ca2a93a9d2101aa712636cbe298448fb4c828f94
Alexis Rincón <marincon444@misena.edu.co>
2022-09-01 05:20:51 -0500

    Punto 1 evidencia

--- Evidencia-01/js/evidencia.js
+++ Evidencia-01/js/evidencia.js
@@ -1,116 +1,26 @@
 // ***************************** //
-//-----------funciones-----------//
+//----------Evidencia------------//
 // ***************************** //
-/*
-function saludar(){
-    console.log("Hola a todos")
-}
-saludar();
-function mayoredad(edad){
-    mayor=(edad>=18)
-    ?alert("Bienvenido al bar")
-    :alert("No puede ingresar");
-}
-mayoredad(prompt("Digite tu edad:"));
-
-*/
-// ***************************** //
-//-----------promedio-----------//
-// ***************************** //
-/*function promedio(sumatoria,cantidad){
-    return(sumatoria/cantidad).toFixed(1);
-}
-let respuesta=true, notas=0, i=1;
-while(respuesta){
-    nota = parseFloat(prompt(`Ingrese la calificación`));
-    notas=notas+nota;
-    respuesta=confirm("Desea agregar otra calificación");
-    i++;
-}
-alert(`El promedio es ${promedio(notas,i)}`)*/
-
-// ***************************** //
-//-----------promedio-----------//
-// ***************************** //
-/*let promedio=(sumatoria,cantidad)=>{
-    return(sumatoria/cantidad).toFixed(1);
-}
-let cantidad = parseInt(prompt("Ingrese la cantidad de notas:")), total=0;
-
-for(let dato=1; dato<=cantidad; dato++){
-     nota = parseFloat(prompt(`Ingrese la calificación numero ${dato}`));
-     total=total+nota;
- }
- alert(`El promedio es ${promedio(total,cantidad)}`)*/
-
- // ***************************** //
-//-----------edad mayor-----------//
-// ***************************** //
-
-/*let mayoredad=function(edad){
-    mayor=(edad>=18)
-    ?alert("Bienvenido al bar")
-    :alert("No puede ingresar");
-}
-mayoredad(prompt("Digite tu edad:"));*/
-
- // ***************************** //
-//-----------Objetos-------------//
-// ***************************** //
-/*const mascota={
-    nombre:"Feliz",
-    tipo: "gato",
-    edad: 4,
-    comer(){
-        console.log("El gato está comiendo")
+function funcion(numero){
+    let name =document.getElementById("name").value;
+    if(name==null){
+        alert("Digite si nombre primero");
+    }else{
+        switch (numero) {
+            case 1:
+                alert(name.toUpperCase());
+            break;
+            case 2:
+                alert(name.toLowerCase());
+            break;
+            case 3:
+                alert(name.length);
+            break;
+            case 4:
+                alert(name.charAt());
+            break;
+            default:
+                break;
+        }
     }
 }
-
-mascota.comer()*/
-
- // ***************************** //
-//--------Area de figura----------//
-// ***************************** //
-/*
-const cuadrado={
-    lado:12,
-    area(){
-        operacion=cuadrado.lado*cuadrado.lado;
-        console.log("El area del cuadrado es "+operacion);
-    }
-}
-cuadrado.area();
-
-const circulo={
-    radio:parseFloat( prompt("Digite el radio del circulo: ")),
-    area(){
-        operacion=3.14*(this.radio^2);
-        console.log("El area del circulo es "+operacion);
-        alert("El area del circulo es "+operacion);
-    },
-    setradio(radio){
-        this.radio=radio;
-    }
-}
-circulo.radio;
-circulo.area();
-*/
- // ***************************** //
-//--------total de compra---------//
-// ***************************** //
-
-let producto=prompt("Digite el nombre del producto:");
-let cantidad=parseInt(prompt("Digite la cantidad de "+producto+":"));
-let precio=parseInt(prompt("Digite el precio de "+producto+":"));
-
-const totales={
-    calculartotal(){
-        let total=precio*cantidad;
-        console.log(`El total es ${total}`);
-
-    }
-}
-
-totales.cantidad=cantidad;
-totales.precio=precio;
-totales.calculartotal();
